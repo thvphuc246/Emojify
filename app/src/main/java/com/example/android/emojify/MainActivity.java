a5c2cf44cd21aaab52976e5b1e2b7b550972ac4e
thvphuc246 <truonghoangvinhphuc@gmail.com>
2018-04-10 19:03:44 +0300

    Revert to e2f19f826fc0ade719165ed00e5dabf73aee646d

--- app/src/main/java/com/example/android/emojify/MainActivity.java
+++ app/src/main/java/com/example/android/emojify/MainActivity.java
@@ -39,10 +39,6 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -50,14 +46,14 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String FILE_PROVIDER_AUTHORITY = "com.example.android.fileprovider";
 
-    @BindView(R.id.image_view) ImageView mImageView;
+    private ImageView mImageView;
 
-    @BindView(R.id.emojify_button) Button mEmojifyButton;
-    @BindView(R.id.share_button) FloatingActionButton mShareFab;
-    @BindView(R.id.save_button) FloatingActionButton mSaveFab;
-    @BindView(R.id.clear_button) FloatingActionButton mClearFab;
+    private Button mEmojifyButton;
+    private FloatingActionButton mShareFab;
+    private FloatingActionButton mSaveFab;
+    private FloatingActionButton mClearFab;
 
-    @BindView(R.id.title_text_view) TextView mTitleTextView;
+    private TextView mTitleTextView;
 
     private String mTempPhotoPath;
 
@@ -69,7 +65,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ButterKnife.bind(this);
+        // Bind the views
+        mImageView = (ImageView) findViewById(R.id.image_view);
+        mEmojifyButton = (Button) findViewById(R.id.emojify_button);
+        mShareFab = (FloatingActionButton) findViewById(R.id.share_button);
+        mSaveFab = (FloatingActionButton) findViewById(R.id.save_button);
+        mClearFab = (FloatingActionButton) findViewById(R.id.clear_button);
+        mTitleTextView = (TextView) findViewById(R.id.title_text_view);
     }
 
     /**
@@ -77,7 +79,6 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view The emojify me button.
      */
-    @OnClick(R.id.emojify_button)
     public void emojifyMe(View view) {
         // Check for the external storage permission
         if (ContextCompat.checkSelfPermission(this,
@@ -180,9 +181,6 @@ public class MainActivity extends AppCompatActivity {
         // Resample the saved image to fit the ImageView
         mResultsBitmap = BitmapUtils.resamplePic(this, mTempPhotoPath);
 
-        //Detect the faces and overlay the emoji
-        mResultsBitmap = Emojifier.detectFacesAndOverlayEmoji(this, mResultsBitmap);
-
         // Set the new bitmap to the ImageView
         mImageView.setImageBitmap(mResultsBitmap);
     }
@@ -193,7 +191,6 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view The save button.
      */
-    @OnClick(R.id.save_button)
     public void saveMe(View view) {
         // Delete the temporary image file
         BitmapUtils.deleteImageFile(this, mTempPhotoPath);
@@ -207,7 +204,6 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view The share button.
      */
-    @OnClick(R.id.share_button)
     public void shareMe(View view) {
         // Delete the temporary image file
         BitmapUtils.deleteImageFile(this, mTempPhotoPath);
@@ -224,7 +220,6 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param view The clear button.
      */
-    @OnClick(R.id.clear_button)
     public void clearImage(View view) {
         // Clear the image and toggle the view visibility
         mImageView.setImageResource(0);
